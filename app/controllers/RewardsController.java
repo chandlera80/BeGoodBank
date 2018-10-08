900a6f6d770d0cafb33148c243047e08328fe79d
Chandlera <chandlera80@gmail.com>
2018-10-08 14:14:48 -0500

    cleaned up with HTML

--- app/controllers/RewardsController.java
+++ app/controllers/RewardsController.java
@@ -1,13 +1,16 @@
 package controllers;
 
 
+import models.Child;
 import models.RewardOptions;
 import play.db.jpa.JPAApi;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.helper.form;
 
 import javax.inject.Inject;
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -28,6 +31,7 @@ import java.util.List;
             String sql = "SELECT r FROM RewardOptions r ORDER BY rewardType";
 
             List<RewardOptions> rewardTypes = jpaApi.em().createQuery(sql, RewardOptions.class).getResultList();
+
 
             return ok(views.html.rewardOptions.render(rewardTypes));
         }
